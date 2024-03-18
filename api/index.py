fb103beeaa2c294be929a519cb027b0c014d418c
MintLatte77 <74120838+MintLatte77@users.noreply.github.com>
2024-03-18 19:42:13 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -10,8 +10,4 @@ def home():
 def about():
     return 'About'
 
-@app.route('/meal')
-def meal():
-    json_data = request.get_json()
-    timezone = json_data['userRequest']['timezone']
-    return {"version": "2.0","template":{"outputs": [{"simpleText": {"text": json_data}}]}}
+
