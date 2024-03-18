ea687d64e43a7c710c58cee816f10fca69849617
MintLatte77 <74120838+MintLatte77@users.noreply.github.com>
2024-03-18 19:34:29 +0900

    Update index.py

--- api/index.py
+++ api/index.py
@@ -15,5 +15,3 @@ def meal():
     json_data = request.get_json()
     timezone = json_data['userRequest']['timezone']
     return {"version": "2.0","template":{"outputs": [{"simpleText": {"text": json_data}}]}}
-
-app.run("0.0.0.0")
